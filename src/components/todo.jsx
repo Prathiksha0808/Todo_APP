71ca614997583f8b18f0e741d3e326e8cc95a0fe
Prathiksha <kotianprathiksha08@gmail.com>
2026-01-05 15:26:50 +0530

    add delete todo functionality

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -3,76 +3,96 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
-function Todo(){
+function Todo() {
 
-  const[task,setTask]=useState("");
-  const[date,setDate]=useState("");
-  const[time,setTime]=useState("");
-  const[todos,setTodo]=useState([]);
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodo] = useState([]);
 
-  const handleAddTask=()=>{
-    const newTask={
-      id:Date.now(),
+  const handleAddTask = () => {
+    const newTask = {
+      id: Date.now(),
       task,
       date,
       time,
     };
-    setTodo([...todos,newTask]);
+    setTodo([...todos, newTask]);
     setTask("");
     setDate("");
     setTime("");
   };
- 
-return(
+
+  const handleDelete=(id)=>{
+    setTodo(todos.filter((todo)=>todo.id!==id));
+  }
+
+  return (
     <Container maxWidth="sm">
-    <Box
-      sx={{mt: 4,p: 3,
-        border: "1px solid grey",
-        borderRadius: 2,display: "flex",
-        flexDirection: "column",gap: 2,
-      }}
-    >
-      <Typography variant="h4" align="center">
-        Todo App
-      </Typography>
+      <Box
+        sx={{
+          mt: 4, p: 3,
+          border: "1px solid grey",
+          borderRadius: 2, display: "flex",
+          flexDirection: "column", gap: 2,
+        }}
+      >
+        <Typography variant="h4" align="center">
+          Todo App
+        </Typography>
 
-      <TextField
-        label="Enter a Task" variant="outlined" fullWidth value={task} onChange={(e)=>setTask(e.target.value)}
-      />
+        <TextField
+          label="Enter a Task" variant="outlined" fullWidth value={task} onChange={(e) => setTask(e.target.value)}
+        />
 
-      <TextField
-        label="Enter a deadline" type="date" variant="outlined" fullWidth  value={date} onChange={(e)=>setDate(e.target.value)} InputLabelProps={{ shrink: true }}
-      />
+        <TextField
+          label="Enter a deadline" type="date" variant="outlined" fullWidth value={date} onChange={(e) => setDate(e.target.value)} InputLabelProps={{ shrink: true }}
+        />
 
-       <TextField
-        label="Enter a deadline" type="time" variant="outlined" fullWidth  value={time} onChange={(e)=>setTime(e.target.value)} InputLabelProps={{ shrink: true }}
+        <TextField
+          label="Enter a deadline" type="time" variant="outlined" fullWidth value={time} onChange={(e) => setTime(e.target.value)} InputLabelProps={{ shrink: true }}
 
-      />
+        />
 
-      <Button variant="contained" size="large" onClick={handleAddTask}>
-        Add
-      </Button>
-      {todos.map((todo) => (
-  <Box
-    key={todo.id}
-    sx={{
-      border: "1px solid #ccc",
-      p: 2,
-      borderRadius: 1,
-    }}
-  >
-    <Typography>{todo.task}</Typography>
-    <Typography variant="caption">
-      {todo.date} {todo.time}
-    </Typography>
-  </Box>
-))}
+        <Button variant="contained" size="large" onClick={handleAddTask}>
+          Add
+        </Button>
+        {todos.map((todo) => (
+          <Box
+            key={todo.id}
+            sx={{
+              border: "1px solid #ccc",
+              p: 2,
+              borderRadius: 1,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Typography>{todo.task}</Typography>
+              <Typography variant="caption">
+                {todo.date} {todo.time}
+              </Typography>
+            </Box>
 
-    </Box>
-  </Container>
-)
+            <Box sx={{ display: "flex", gap: 1 }}>
+              <Button size="small" variant="outlined">
+                Edit
+              </Button>
+              <Button size="small" color="error" variant="outlined" onClick={()=>handleDelete(todo.id)}>
+                Delete
+              </Button>
+            </Box>
+          </Box>
+        ))}
+
+
+      </Box>
+    </Container>
+  )
 }
 export default Todo
